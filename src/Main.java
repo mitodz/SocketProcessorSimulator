5e81c120800753cb76b3a0e0b51d28eced5c7f07
mitodz <mitodzzz@gmail.com>
2019-03-17 21:02:19 +0300

    ОТЛАДКА...

--- src/Main.java
+++ src/Main.java
@@ -1,37 +1,58 @@
-import javafx.util.Pair;
-
-import java.util.ArrayDeque;
-import java.util.Deque;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner("1 2\n" +
-                "0 1\n" +
-                "1 1");
+        Scanner scanner = new Scanner("1 25\n" +
+                "16 0\n" +
+                "29 3\n" +
+                "44 6\n" +
+                "58 0\n" +
+                "72 2\n" +
+                "88 8\n" +
+                "95 7\n" +
+                "108 6\n" +
+                "123 9\n" +
+                "139 6\n" +
+                "152 6\n" +
+                "157 3\n" +
+                "169 3\n" +
+                "183 1\n" +
+                "192 0\n" +
+                "202 8\n" +
+                "213 8\n" +
+                "229 3\n" +
+                "232 3\n" +
+                "236 3\n" +
+                "239 4\n" +
+                "247 8\n" +
+                "251 2\n" +
+                "267 7\n" +
+                "275 7");
         int b = scanner.nextInt();
         int n = scanner.nextInt();
         int time = 0;
         int dur = 0;
-        Deque<Pair<Integer,Integer>> bQ = new ArrayDeque<>();
+        Deque<HashMap.Entry<Integer,Integer>> bQ = new ArrayDeque<>();
         for (int i = 0; i < n; i++) {
             int t = scanner.nextInt();
             int d = scanner.nextInt();
-            bQ.addLast(new Pair<>(t,d));
-            //if (bQ.size()<b && i < n - 1) continue;
-            if (!bQ.isEmpty()) {
-                Pair<Integer, Integer> temp = bQ.pollFirst();
-                if (t<time) {
-                    System.out.println(-1);
-                    continue;
-                }
-                time += temp.getKey() + dur;
+            if (t<time) {
+                System.out.print(-1 + " ");//ФИНАЛЬНЫЙ РЕЗУЛЬТАТ С НОВОЙ СТРОКИ!!!
+                continue;
+            }
+            bQ.addLast(new HashMap.SimpleEntry<>(t,d));
+            if (bQ.size()<b && i < n - 1) continue;
+            while (!bQ.isEmpty()) {
+                HashMap.Entry<Integer, Integer> temp = bQ.pollFirst();
+                //time += temp.getKey() + dur;
+                time = temp.getKey() + dur;
+                System.out.print(time + " ");//ФИНАЛЬНЫЙ РЕЗУЛЬТАТ С НОВОЙ СТРОКИ!!!
                 dur = temp.getValue();
-                System.out.println(time);
+                time = temp.getKey() + dur;
+                //16 29 44 58 72 88 -1 108 123 139 152 -1 169 183 192 202 213 229 232 236 239 247 -1 267 275
             }
 
         }
-
     }
 }
